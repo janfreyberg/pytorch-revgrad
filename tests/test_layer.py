4a6043bc957066b204152da877b83146b5c8b6be
Jan Freyberg <jan.freyberg@gmail.com>
2019-03-30 12:08:41 +0000

    add test that gradients are reversed

---
+++ tests/test_layer.py
@@ -0,0 +1,21 @@
+import copy
+import torch
+from pytorch_revgrad import RevGrad
+
+
+def test_gradients_inverted():
+    network = torch.nn.Sequential(torch.nn.Linear(5, 3), torch.nn.Linear(3, 1))
+    revnetwork = torch.nn.Sequential(copy.deepcopy(network), RevGrad())
+
+    inp = torch.randn(8, 5)
+    outp = torch.randn(8)
+
+    criterion = torch.nn.MSELoss()
+
+    criterion(network(inp), outp).backward()
+    criterion(revnetwork(inp), outp).backward()
+
+    assert all(
+        (p1.grad == -p2.grad).all()
+        for p1, p2 in zip(network.parameters(), revnetwork.parameters())
+    )
